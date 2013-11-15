845fc409c5d9731edaeed0b41d3c055bd5788fe5
morimoto <b2020@oic.jp>
2013-11-15 18:58:50 +0900

    とりあえず作業保存

--- server/oicsns/src/main/java/com/oic/connection/Connection.java
+++ server/oicsns/src/main/java/com/oic/connection/Connection.java
@@ -6,10 +6,24 @@
 
 package com.oic.connection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.eclipse.jetty.websocket.api.Session;
+
 /**
  *
  * @author B2020
  */
 public class Connection {
+    private List<Session> sessions = new ArrayList<Session>();
+    private HashMap<Integer, Session> connects = new HashMap<Integer, Session>();
+    private HashMap<Integer, String> authKey = new HashMap<Integer, String>();
     
+    public void addSession(Session session){
+        this.sessions.add(session);
+        for(Session s : this.sessions){
+            System.out.println(s.getRemoteAddress());
+        }
+    }
 }
--- server/oicsns/src/main/java/com/oic/net/WebSocketListener.java
+++ server/oicsns/src/main/java/com/oic/net/WebSocketListener.java
@@ -6,6 +6,7 @@
 
 package com.oic.net;
 
+import com.oic.connection.Connection;
 import com.sun.org.apache.xalan.internal.xsltc.runtime.BasisLibrary;
 import java.util.logging.Logger;
 import org.eclipse.jetty.websocket.api.Session;
@@ -21,25 +22,20 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 @WebSocket
 public class WebSocketListener {
     private static final Logger LOG = Logger.getLogger(WebSocketListener.class.getName());
-    
-    private Session session;
+    static Connection con = new Connection();
     
     @OnWebSocketConnect
     public void onConnect(Session session){
-        this.session = session;
-        System.out.println("connect is " + session.getRemoteAddress());
+        LOG.info("connection :" + session.getRemoteAddress());
+        con.addSession(session);
     }
     @OnWebSocketMessage
     public void onText(String message){
-        System.out.println(session.getRemoteAddress() + ": " + message);
+      //  System.out.println(session.getRemoteAddress() + ": " + message);
     }
     
     @OnWebSocketClose
     public void onClose(int statusCode, String reason){
         
-    }
-    
-    public Session getSession(){
-        return this.session;
     }
 }
