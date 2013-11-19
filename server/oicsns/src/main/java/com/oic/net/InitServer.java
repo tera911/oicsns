a371b205eacc4d1d7e7c24ac1c0e54c09ba6349a
morimoto <b2020@oic.jp>
2013-11-19 16:25:33 +0900

    Mapのxmlを作成中

--- server/oicsns/src/main/java/com/oic/net/InitServer.java
+++ server/oicsns/src/main/java/com/oic/net/InitServer.java
@@ -6,19 +6,27 @@
 
 package com.oic.net;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 /**
- *
- * @author b2020
+ * サーバー起動時に実行されるクラス
+ * @author Morimoto
  */
 public class InitServer extends HttpServlet{
-
     @Override
     public void init() throws ServletException {
-        super.init(); //To change body of generated methods, choose Tools | Templates.
+        super.init();
         System.err.println("Init.");
+        try(InputStream is = getClass().getResourceAsStream("/map/3A.xml")){
+           int Byte;
+           while((Byte = is.read()) != -1){
+               System.out.println((char)Byte);
+           }
+        }catch(IOException e){}
     }
     
 }
