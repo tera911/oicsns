7086ac3d03fb49f5d2279c0efdf40b99a17e8a44
morimoto <b2020@oic.jp>
2013-12-16 19:46:42 +0900

    登録してないユーザを自動登録する機能を追加
    ログインIDが登録されていなければ自動で登録

--- oicsns/src/main/java/com/oic/login/TestLoginHandler.java
+++ oicsns/src/main/java/com/oic/login/TestLoginHandler.java
@@ -4,10 +4,12 @@
  */
 package com.oic.login;
 
+import com.oic.client.AutoRegister;
 import com.oic.connection.Connections;
 import com.oic.event.ActionEventImpl;
 import com.oic.net.WebSocketListener;
 import com.oic.utils.DatabaseConnection;
+import com.oic.utils.Validators;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,50 +28,64 @@ public class TestLoginHandler implements ActionEventImpl{
     private static final Logger LOG = Logger.getLogger(TestLoginHandler.class.getName());
     @Override
     public void ActionEvent(JSONObject json, WebSocketListener webSocket) {
-        String accesstoken;
-        String accesstokensecret;
-        
+        JSONObject loginStatus = new JSONObject();
+        loginStatus.put("method", "loginstatus");
         //validation
-        if(json.get("userid") == null || json.get("password") == null){
-            LOG.warning("invalid Message!");
+        if(!validation(json)){
+            loginStatus.put("status", "1");
+            webSocket.sendJson(loginStatus);
             return;
         }
-        
+        String accesstoken;
+        String accesstokensecret;
         accesstoken = json.get("userid").toString(); //JSONからデータ取り出し
-        accesstokensecret = json.get("password").toString();
+        accesstokensecret = json.get("password").toString(); //JSONからデータ取り出し
         
         long userId = -1;
-        try{
-            userId = login(accesstoken, accesstokensecret);
-        }catch(SQLException e){
-            LOG.log(Level.WARNING, "SQL Exception : {0}",e);
-        }
+        userId = login(accesstoken, accesstokensecret);//ログイン出来た場合はuseridが入る
         
-        JSONObject loginStatus = new JSONObject();
-        if(userId > 0){//ログイン成功
+        if(userId  > 0){//ログイン成功
             webSocket.userLogin(userId);
-            loginStatus.put("status", "1");
+            loginStatus.put("status", "0");
             LOG.log(Level.INFO, "Login Success.");
         }else{  //ログイン失敗
-            loginStatus.put("status", "0");
+            loginStatus.put("status", "1");
             LOG.log(Level.INFO, "Login Faild.");  
         }
         webSocket.sendJson(loginStatus);
     }
     
-    public long login(String accesstoken, String accesstokensecret) throws SQLException{
+    /**
+     * ログインに成功したらユーザIDが返ってくる<br>
+     * 新規IDの場合は自動で登録
+     * @param accessToken
+     * @param accessTokenSecret
+     * @return 
+     */
+    public long login(String accessToken, String accessTokenSecret){
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
+        String sql;
         try{
             con = DatabaseConnection.getConnection();
+            sql = "SELECT userid FROM user WHERE accesstoken = ? AND accesstokensecret = ?";
             Long userId;
-            ps = con.prepareStatement("SELECT userid FROM user WHERE accesstoken = ? AND accesstokensecret = ?");
-            ps.setString(1, accesstoken);
-            ps.setString(2, accesstokensecret);
+            ps = con.prepareStatement(sql);
+            ps.setString(1, accessToken);
+            ps.setString(2, accessTokenSecret);
             rs = ps.executeQuery();
             if(!rs.next()){
-                return -1;
+                sql = "SELECT userid FROM user WHERE accesstoken = ?";
+                ps = con.prepareStatement(sql);
+                ps.setString(1, accessToken);
+                rs = ps.executeQuery();
+                if(!rs.next()){
+                    return -1;
+                }else{
+                    new AutoRegister(accessToken, accessTokenSecret).register();
+                    return 0;
+                }
             }
             userId = rs.getLong("userid");
             return userId;
@@ -80,4 +96,10 @@ public class TestLoginHandler implements ActionEventImpl{
         }       
     }
     
+    public boolean validation(JSONObject json){
+        Validators v = new Validators(json);
+        v.add("userid" , v.required());
+        v.add("password", v.required()); 
+        return v.validate();
+    }
 }
