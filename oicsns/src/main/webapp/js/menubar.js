7958a34979342508a6f39ba7f2dbf1357b161976
inamoto <b2248@oic.jp>
2013-12-17 15:04:43 +0900

    メニューバー実装！

---
+++ oicsns/src/main/webapp/js/menubar.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+                            //メニューマウスオーバーで白くする処理
+                            $('.icon').hover(
+                                //マウスオーバー
+                                function(){
+                                $(this).css( 'background-color', 'rgba(255,255,255,0.3)' );
+                                },
+                                //マウスアウト
+                                function(){
+                                $(this).css( 'background-color' , 'rgba(255,255,255,0)' );
+                                }
+                            );
+                            //メニューバーを最小化する処理
+                            $("#menu6").click(function(){
+                                $("#maxMenubar").toggle('slide',{direction:'right'},function(){
+                                    $("#minMenubar").toggle('slide',{direction:'right'});
+                                });
+                            });
+                            //メニューバーを最大化する処理
+                            $("#menu7").click(function(){
+                                $("#minMenubar").toggle('slide',{direction:'right'});
+                                $("#maxMenubar").toggle('slide',{direction:'right'});
+                            });
+                        });
